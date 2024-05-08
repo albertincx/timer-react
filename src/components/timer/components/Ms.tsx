343b818601e98593d2c62760642e8e7334308501
albertincx <albertincx@gmail.com>
2024-05-08 18:41:48 +0700

    test init 3

--- src/components/timer/components/Ms.tsx
+++ src/components/timer/components/Ms.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { useRef, useEffect } from "react";
-import MobileSelect, { CustomConfig } from "mobile-select";
+import {useRef, useEffect} from "react";
+import MobileSelect, {CustomConfig} from "mobile-select";
 
 type MSProps = {
     config: Omit<CustomConfig, "trigger">;
@@ -8,16 +8,14 @@ type MSProps = {
     children?: JSX.Element;
 };
 export default function MsComponent(props: MSProps) {
-    const tirggerRef = useRef(null);
+    const triggerRef = useRef(null);
     let instanceRef = useRef<any>(null);
 
     useEffect(() => {
-        console.log(" triggerDisplayValue: !props.children", !props.children);
-        console.log("useEffect:实例化调用", instanceRef);
         if (!instanceRef.current) {
             instanceRef.current = new MobileSelect({
                 ...props.config,
-                trigger: (tirggerRef.current as unknown) as HTMLElement,
+                trigger: (triggerRef.current as unknown) as HTMLElement,
                 triggerDisplayValue: !props.children
             });
         }
@@ -25,14 +23,14 @@ export default function MsComponent(props: MSProps) {
         return () => {
             instanceRef?.current.destroy();
             instanceRef.current = null;
-            console.log("销毁时调用", instanceRef);
+            // console.log("销毁时调用", instanceRef);
         };
     }, []);
 
     return (
         <div>
-            <div className="ms-default-trigger" ref={tirggerRef}>
-                {props.children || props.placeholder || "Please select an option.."}
+            <div className="ms-default-trigger" ref={triggerRef}>
+                {props.children || props.placeholder || "Please select an option..!"}
             </div>
         </div>
     );
