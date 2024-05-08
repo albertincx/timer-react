90338399357b98f2ece1684da5ec30f17f9cac58
albertincx <albertincx@gmail.com>
2024-05-08 18:47:37 +0700

    test init 4

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import {VitePWA} from 'vite-plugin-pwa'
 
-let faviconURL = '/favicon.svg'
+let faviconURL = '/favicon.png'
 export default ({mode}) => {
     const dev = mode === 'development';
     // console.log(mode, dev);
